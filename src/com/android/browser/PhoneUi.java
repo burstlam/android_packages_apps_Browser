9f3ab956e46549b3c504020301c9e1d41b33f7c5
Afzal Najam <afzal411+android@gmail.com>
2012-04-26 01:54:01 -0400

    Added a menu item in Browser called Close other tabs
    This closes all other tabs except the current one (only for the phone interface)
    Will submit patch for tablet interface once this is approved.
    It solves the problem of mass closing unneeded tabs to a certain extent.
    Preferred method would be to select multiple tabs like list items.
    
    Change-Id: I5af00f1c19043104987375f1da396eb6fefb5dd4

--- src/com/android/browser/PhoneUi.java
+++ src/com/android/browser/PhoneUi.java
@@ -427,6 +427,14 @@ public class PhoneUi extends BaseUi implements RealViewSwitcher.OnScreenSwitchLi
         if (incognito != null && !mUseQuickControls) {
             incognito.setVisible(false);
         }
+        MenuItem closeOthers = menu.findItem(R.id.close_other_tabs_id);
+        if (closeOthers != null) {
+            boolean isLastTab = true;
+            if (tab != null) {
+                isLastTab = (mTabControl.getTabCount() <= 1);
+            }
+            closeOthers.setEnabled(!isLastTab);
+        }
         if (showingNavScreen()) {
             menu.setGroupVisible(R.id.LIVE_MENU, false);
             menu.setGroupVisible(R.id.SNAPSHOT_MENU, false);
